07b6b32fb976e1e7c2e11124368c75a6c500cd5f
nehabathuri2772 <BATHURINEHA2772K@GMAIL.COM>
2025-10-06 17:35:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -589,7 +589,8 @@ def build_ui():
 
 if __name__ == "__main__":
     demo = build_ui()
-    demo.queue().launch(server_name="0.0.0.0", server_port=int(os.getenv("PORT", "8015")))
+    demo.queue().launch(server_port=int(os.getenv("PORT", "8015")))
     # demo.launch(server_name="0.0.0.0")
+
 
 
